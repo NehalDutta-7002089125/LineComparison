359f940163c8913dece979f72964f4182ce490e3
NehalDutta-7002089125 <nihal326dutta@gmail.com>
2023-01-05 19:14:13 +0530

    [Nehal]Adding: Measuring the two lines are equal

--- Line-Comparison/LineCompare.cs
+++ Line-Comparison/LineCompare.cs
@@ -10,21 +10,45 @@ namespace Line_Comparison
         {
             double x1, x2, y1, y2;
 
-            Console.WriteLine("Enter the vlue of x : ");
+            double a1, a2, b1, b2;
+
+            Console.WriteLine("Enter the values: ");
 
             x1 = double.Parse(Console.ReadLine());
 
             x2 = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter the value of y : ");
-
             y1 = double.Parse(Console.ReadLine());
 
             y2 = double.Parse(Console.ReadLine());
 
-            double length = Math.Sqrt((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)));
+            Console.WriteLine("Enter the values: ");
 
-            Console.WriteLine("The length between the two point is: " + length);
+            a1 = double.Parse(Console.ReadLine());
+
+            a2 = double.Parse(Console.ReadLine());
+
+            b1 = double.Parse(Console.ReadLine());
+
+            b2 = double.Parse(Console.ReadLine());
+
+            double Length1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+            double Length2 = Math.Sqrt(Math.Pow(a2 - a1, 2) + Math.Pow(b2 - b1, 2));
+
+            if (Length1 == Length2)
+
+            {
+                Console.WriteLine("Both lines are equal.");
+
+            }
+            else
+
+            {
+                Console.WriteLine("Both lines are not equal.");
+
+            }
+
         }
 
     }
